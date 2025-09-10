e90c624820e849620d9cecc8107eab08242f0203
George Le <legeorge4@gmail.com>
2025-09-10 10:00:12 -0400

    Added in web 2

--- apps/web2/app/page.tsx
+++ apps/web2/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World from Web 1</h1>
+        <h1 className="text-2xl font-bold">Hello World from Web 2</h1>
         <Button size="sm">Button</Button>
       </div>
     </div>
